37f2e79d23c89a26c4851b93c2d4aa6916a8fee8
caionakai <kaioesperto@hotmail.com>
2018-03-27 18:25:53 -0300

    teste

---
+++ helloworld.pl
@@ -0,0 +1,8 @@
+#!/usr/local/bin/perl
+use lib 'C:\Users\kaioe\OneDrive\Documentos\Universidade\5º Semestre\Linguagem de Prog\text_adventure_perl';
+use classe;
+
+$object = new Classe("caio", 123456);
+
+
+print "hello world\n";
